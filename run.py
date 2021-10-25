44fbd5b18ca58152d4fb877a2788be4df9128323
Denis-Leparteleg <denislepartelegke@gmail.com>
2021-10-25 13:15:20 +0300

    add the main user logic functionality

--- run.py
+++ run.py
@@ -57,3 +57,77 @@ def check_existing_email(password):
     Function that check if a email exists with that password and return a Boolean
     '''
     return Credentials.credentials_exist(password)
+
+def main():
+    
+    print("Hello Welcome to your credentials list. What is your name?")
+    user_name = input()
+
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+
+    while True:
+        print("Use these short codes : cc - create a new credentials, dc - display credentials, fc -find a credentials, ex -exit the credentials list ")
+        short_code = input().lower()
+
+        if short_code == 'cc':
+            print("New Credentials")
+            print("-"*10)
+
+            print("First name ....")
+            f_name = input()
+
+            print("Last name ...")
+            l_name = input()
+
+            print("Password ...")
+            p_number = input()
+
+            print("Email address ...")
+            e_address = input()
+
+            #     save_credentials(create_credentials(f_name,l_name,password,e_address)) # create and save new credentials.
+            print('\n')
+            print(f"New password {f_name} {l_name} created")
+            print('\n')
+
+        elif short_code == 'dc':
+
+            if display_credentials():
+                print("Here is a list of all your passwords")
+                print('\n')
+
+                for credentials in _all_credentials():
+                    print(
+                    f"{credentials.first_name} {credentials.last_name} .....{credentials.password}")
+
+                    print('\n')
+            else:
+                print('\n')
+                print("You don't have any saved contacts")
+                print('\n')
+
+        elif short_code == 'fc':
+
+            print("Enter the password you want to search for")
+
+            search_password = input()
+            if check_existing_credentials(password):
+                search_credentials = find_password(search_password)
+                print(f"{search_credentials.first_name} {search_credentials.last_name}")
+                print('-' * 20)
+
+                print(f"Password.......{search_credentials.password}")
+                print(f"Email address.......{search_credentials.email}")
+            else:
+                print("The password does not exist")
+
+        elif short_code == "ex":
+            print("Bye .......")
+            break
+        else:
+                print("I didn't get that. kindly use the short codes")
+
+
+if __name__ == '__main__':
+    main()
