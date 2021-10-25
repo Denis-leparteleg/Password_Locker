6be37c0813ff0cb20c239eacedaaccbe868e560d
Denis-Leparteleg <denislepartelegke@gmail.com>
2021-10-25 10:29:13 +0300

    create the setUp method

--- credentials_test.py
+++ credentials_test.py
@@ -4,6 +4,10 @@ import pyperclip
 
 class TestUser(unittest.TestCase):
     '''
-    it will create the Testuser subclass
+    it will create the Testuser subclass.
     '''
-    
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        
